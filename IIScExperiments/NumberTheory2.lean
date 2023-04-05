29636a18e442010dc6840279bc2f84f50b3c9ad9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-04-05 08:39:32 +0530

    add some number theory questions

---
+++ IIScExperiments/NumberTheory2.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2023 Kevin Buzzard. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author : Kevin Buzzard
+-/
+import Mathlib
+
+/-
+
+# Prove the theorem, due to Kraichik, asserting that 13|2^70+3^70
+
+This is the sixth question in Sierpinski's book "250 elementary problems
+in number theory".
+
+-/
+
+example : 13 ∣ 2^70 + 3^70 := by
+  sorry
